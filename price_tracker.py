570830af6ad382157ee245443ad153a9aa9d1353
The Developer <34011883+thedeveloper12@users.noreply.github.com>
2021-11-03 04:13:29 +0300

    Update price_tracker.py

--- price_tracker.py
+++ price_tracker.py
@@ -44,7 +44,7 @@ def start():
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-gpu')
-    driver = webdriver.Chrome(executable_path=f"{Path.cwd}/chromedriver",options=options)
+    driver = webdriver.Chrome(executable_path=f"{Path.cwd()}/chromedriver",options=options)
     wait = WebDriverWait(driver, 10)
     driver.get('https://poocoin.app/tokens/0x079dd74cc214ac5f892f6a7271ef0722f6d0c2e6')
     time.sleep(4)
