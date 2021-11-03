ad9c44bcee30e19159baa9f4a377327a14e672ee
thedeveloper12 <dphenix4570@gmail.com>
2021-11-03 17:15:34 +0300

    a

--- price_tracker.py
+++ price_tracker.py
@@ -4,6 +4,7 @@ from pathlib import Path
 from dotenv import load_dotenv
 import logging
 from telethon.sync import TelegramClient,Button
+import undetected_chromedriver.v2 as uc
 from selenium import webdriver
 from pathlib import Path
 from selenium.webdriver.support.ui import WebDriverWait
@@ -39,13 +40,13 @@ bot = TelegramClient('NasadogeBot',API_KEY,API_HASH).start(bot_token=BOT_TOKEN)
 
 
 def start():
-    options = webdriver.ChromeOptions()
-    options.binary_location = GOOGLE_CHROME_BIN
+    options = uc.ChromeOptions()
+    # options.binary_location = GOOGLE_CHROME_BIN
     options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36")
     options.add_argument('--start-maximize')
     options.add_argument('--headless')
     options.add_argument('--no-sandbox')
-    driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=options)
+    driver = uc.Chrome(options=options)
     driver.set_window_size(1920, 1080)
     wait = WebDriverWait(driver, 10)
     driver.get('https://poocoin.app/tokens/0x079dd74cc214ac5f892f6a7271ef0722f6d0c2e6')
@@ -59,43 +60,39 @@ def start():
     html.send_keys(Keys.PAGE_DOWN)
     html.send_keys(Keys.PAGE_DOWN)
     html.send_keys(Keys.PAGE_DOWN)
-    driver.get_screenshot_as_file('photo.jpg')
-    chat = bot.get_entity("https://t.me/nasadoge_buy_sell")
-    bot.send_file(chat, open('photo.jpg','rb'))
-    # old_sold = ""
-    # while True:
-    #     first_line = wait.until(ec.presence_of_element_located((By.XPATH, '/html/body/div[1]/div/div[1]/div[2]/div/div[2]/div[2]/div/div[3]/div[1]/div/div[2]/div/div[1]')))
-    #     a = first_line.find_elements_by_tag_name('div')
-    #     tx = a[5].text.split('\n')[0]
-    #     if old_sold == tx:
-    #         continue
-    #     old_sold = tx
-    #     types = a[0].text.replace('\n','')
-    #     amount = a[1].text.split('\n')[0]
-    #     price = a[2].text.split('\n')[0]
-    #     token_price = a[3].text.split('\n')[0]
-    #     times = a[4].text.replace('\n','')
-    #     tx_url = a[5].find_element_by_tag_name('a').get_attribute('href')
-        
-    #     if types == 'Buy':
-    #         emoji = "🟢"
-    #     else:
-    #         emoji = "🔴"
-    #     text = (
-    #         f"**New {types} Transaction!** {emoji}\n"
-    #         f"**Time**: `{times}`\n"
-    #         f"**Amount:** `{amount}`\n"
-    #         f"**Price:** `{price}`\n"
-    #         f"**Sold By** `{token_price}`\n"
-    #     )
-    #     keyboard = [
-    #     [  
-    #         Button.url(text='Txn Hash',url=tx_url)
-    #     ]
-    #     ]
+    old_sold = ""
+    while True:
+        first_line = wait.until(ec.presence_of_element_located((By.XPATH, '/html/body/div[1]/div/div[1]/div[2]/div/div[2]/div[2]/div/div[3]/div[1]/div/div[2]/div/div[1]')))
+        a = first_line.find_elements_by_tag_name('div')
+        tx = a[5].text.split('\n')[0]
+        if old_sold == tx:
+            continue
+        old_sold = tx
+        types = a[0].text.replace('\n','')
+        amount = a[1].text.split('\n')[0]
+        price = a[2].text.split('\n')[0]
+        token_price = a[3].text.split('\n')[0]
+        times = a[4].text.replace('\n','')
+        tx_url = a[5].find_element_by_tag_name('a').get_attribute('href')
+        if types == 'Buy':
+            emoji = "🟢"
+        else:
+            emoji = "🔴"
+        text = (
+            f"**New {types} Transaction!** {emoji}\n"
+            f"**Time**: `{times}`\n"
+            f"**Amount:** `{amount}`\n"
+            f"**Price:** `{price}`\n"
+            f"**Sold By** `{token_price}`\n"
+        )
+        keyboard = [
+        [  
+            Button.url(text='Txn Hash',url=tx_url)
+        ]
+        ]
 
-    #     chat = bot.get_entity("https://t.me/nasadoge_buy_sell")
-    #     bot.send_message(chat, message=text,buttons=keyboard)
+        chat = bot.get_entity("https://t.me/nasadoge_buy_sell")
+        bot.send_message(chat, message=text,buttons=keyboard)
         
 
 if __name__ == "__main__":
